b5280666c13fdf0e923e1b0364f30429b9e4a746
Dimash <murathandinmuhammed@gmail.com>
2026-02-25 12:00:49 +0500

    first commit

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	db, err := sql.Open("sqlite3", "quotes.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Database connected")
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /api/quotes/", quotesListDispatcher)
@@ -18,4 +33,5 @@ func main() {
 	if err := http.ListenAndServe(":9090", mux); err != nil {
 		log.Fatal(err)
 	}
+
 }
